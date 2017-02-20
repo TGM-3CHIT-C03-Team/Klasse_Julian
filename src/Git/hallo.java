942a0d7c892a8bcdbcc337be6ed3d47cb278b7db
jbrzakala <jbrzakala@student.tgm.ac.at>
2017-02-20 11:16:43 +0100

    commit01

--- src/Git/hallo.java
+++ src/Git/hallo.java
@@ -2,8 +2,12 @@ package Git;
 
 public class hallo {
 
-	public static void main(String[] args) {
+	public static void hallo() {
 		System.out.println("Julian lässt schön grüßen :-) !");
+	}
+	
+	public static void main(String []args){
+		hallo();
 	}
 
 }
