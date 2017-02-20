3bedcdbf9596964c9215c1f8e6fe4b7222411f4e
jchelapurath <jchelapurath@student.tgm.ac.at>
2017-02-20 13:06:42 +0100

    Noten Methode

--- src/Git/hallo.java
+++ src/Git/hallo.java
@@ -1,10 +1,24 @@
 package Git;
 
+import javax.swing.JOptionPane;
+
 public class hallo {
 
 	public static void hallo() {
 		System.out.println("Julian lässt schön grüßen :-) !");
 		System.out.println("Mario hier");
+		if(NoteJerin() == 1) {
+			JOptionPane.showMessageDialog(null, "Jerin ist der Beste");
+		}else{
+			JOptionPane.showMessageDialog(null, "Jerin ist immernoch besser als Julian, obwohl meine Note eine" + NoteJerin() + " ist : )");
+		}
+	}
+	
+	
+	public static int NoteJerin () {
+		double not = (Math.random() * 4)+1;
+		
+		return (int)not;
 	}
 	
 	public static void main(String []args){
