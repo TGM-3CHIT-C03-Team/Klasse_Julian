4cd408b7e4c39789657e8a7b85e20e2aebe24708
mfentler <mfentler@student.tgm.ac.at>
2017-02-20 12:43:51 +0100

    Konsolenausgabe hinzugefügt

--- src/Git/hallo.java
+++ src/Git/hallo.java
@@ -4,6 +4,7 @@ public class hallo {
 
 	public static void hallo() {
 		System.out.println("Julian lässt schön grüßen :-) !");
+		System.out.println("Mario hier");
 	}
 	
 	public static void main(String []args){
