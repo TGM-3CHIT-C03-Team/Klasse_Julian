f966d0765a083734f5ea0c3df8348169d1ce4ec4
jbrzakala <jbrzakala@student.tgm.ac.at>
2017-02-27 10:12:43 +0100

    merge

--- src/Git/hallo.java
+++ src/Git/hallo.java
@@ -1,9 +1,26 @@
 package Git;
 
+import javax.swing.JOptionPane;
+
 public class hallo {
 
 	public static void hallo() {
 		System.out.println("Julian lässt schön grüßen :-) !");
+		System.out.println("Mario hier");
+		System.out.println("molbrich lässt euch grüßen");
+
+		if(NoteJerin() == 1) {
+			JOptionPane.showMessageDialog(null, "Jerin ist der Beste");
+		}else{
+			JOptionPane.showMessageDialog(null, "Jerin ist immernoch besser als Julian, obwohl meine Note eine" + NoteJerin() + " ist : )");
+		}
+	}
+	
+	
+	public static int NoteJerin () {
+		double not = (Math.random() * 4)+1;
+		
+		return (int)not;
 	}
 	
 	public static void main(String []args){
